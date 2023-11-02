12ed8546c4986ff94c05627206f603d42532a3ad
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-02 18:26:03 -0400

    set up nav bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
+import TopicList from 'components/TopicList';
+import FavBadge from './FavBadge';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
+
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList />
+      {/* need  to pass a fx to the FavBadge to red the heart*/}
+      <FavBadge isFavPhotoExist /> 
+
     </div>
   )
 }
